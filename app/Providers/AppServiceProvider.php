aafb1bca9cfa1b5e3858ad38102f48dc68245864
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2019-11-21 12:49:43 +0100

    Trying to fix migration error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,6 +13,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
+    
+    public function boot()
+    {
+    Schema::defaultStringLength(191);
+    }
+
     public function register()
     {
         //
