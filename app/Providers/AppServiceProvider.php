fe4a084f45a00ea451aaff32f472651d78b43cde
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2019-11-21 12:52:47 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
-
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,12 +11,6 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    
-    public function boot()
-    {
-    Schema::defaultStringLength(191);
-    }
-
     public function register()
     {
         //
